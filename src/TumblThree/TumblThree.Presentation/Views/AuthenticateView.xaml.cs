94a9b1adc8a39d9892c333d7f3d50912ccd93ae1
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2016-10-08 20:59:18 +0200

    Ignore js error popups in .net browser
    Ignore js error popups during oauth authentication (tumblr) with the
    built-in .net browser.

--- src/TumblThree/TumblThree.Presentation/Views/AuthenticateView.xaml.cs
+++ src/TumblThree/TumblThree.Presentation/Views/AuthenticateView.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Waf.Applications;
@@ -31,12 +33,13 @@ namespace TumblThree.Presentation.Views
         {
             InitializeComponent();
             this.viewModel = new Lazy<AuthenticateViewModel>(() => ViewHelper.GetViewModel<AuthenticateViewModel>(this));
-
             browser.Navigated += Browser_Navigated;
         }
 
         private void Browser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
         {
+            SetSilent(browser, true); // make it silent, no js error popus.
+
             try
             {
                 WebBrowser wb = (WebBrowser)sender;
@@ -50,9 +53,33 @@ namespace TumblThree.Presentation.Views
             }
         }
 
-        private void Browser_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        public static void SetSilent(WebBrowser browser, bool silent)
         {
+            if (browser == null)
+                throw new ArgumentNullException("browser");
 
+            // get an IWebBrowser2 from the document
+            IOleServiceProvider sp = browser.Document as IOleServiceProvider;
+            if (sp != null)
+            {
+                Guid IID_IWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+                Guid IID_IWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
+
+                object webBrowser;
+                sp.QueryService(ref IID_IWebBrowserApp, ref IID_IWebBrowser2, out webBrowser);
+                if (webBrowser != null)
+                {
+                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { silent });
+                }
+            }
+        }
+
+
+        [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IOleServiceProvider
+        {
+            [PreserveSig]
+            int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
         }
 
         private AuthenticateViewModel ViewModel { get { return viewModel.Value; } }
