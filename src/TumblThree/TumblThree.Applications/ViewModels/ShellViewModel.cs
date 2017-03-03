1381e67a2d3b4c679e4dd5d149f15af861c03db2
amigre38 <amigre38@gmail.com>
2017-03-03 09:37:00 +0100

    Update ShellViewModel.cs
    I made a modification to handle an exception that make TumblThree crashing.

--- src/TumblThree/TumblThree.Applications/ViewModels/ShellViewModel.cs
+++ src/TumblThree/TumblThree.Applications/ViewModels/ShellViewModel.cs
@@ -129,8 +129,10 @@ namespace TumblThree.Applications.ViewModels
 
         public void ShowError(Exception exception, string message)
         {
+            string ErrorMessageItem1;
             var errorMessage = new Tuple<Exception, string>(exception, message);
-            if (!errors.Any(error => error.Item1.ToString() == errorMessage.Item1.ToString() && error.Item2 == errorMessage.Item2))
+            ErrorMessageItem1 = errorMessage.Item1?.ToString() ?? "";
+            if (!errors.Any(error => error.Item1.ToString() == ErrorMessageItem1 && error.Item2 == errorMessage.Item2))
                 errors.Add(errorMessage);
         }
 
