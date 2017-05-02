f7c9bbb015f00ddb7cea184487c6927b817ae218
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-05-02 08:51:58 +0200

    Code refactoring

---
+++ src/TumblThree/TumblThree.Domain/Models/TumblrLikeByBlog.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace TumblThree.Domain.Models
+{
+    [DataContract]
+    public class TumblrLikeByBlog : Blog
+    {
+        public TumblrLikeByBlog(string url, string location, BlogTypes blogType) : base(url, location, blogType)
+        {
+        }
+
+        protected override string ExtractName()
+        {
+            return Url.Split('/')[5];
+        }
+
+        protected override string ExtractUrl()
+        {
+            int index = Url.Split('/')[5].Length;
+            var lengthOfUrl = 32;
+            return Url.Substring(0, index + lengthOfUrl);
+        }
+    }
+}
