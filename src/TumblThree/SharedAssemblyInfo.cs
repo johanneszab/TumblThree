7272a8f45974f7813088dc97dfb5882529ee325f
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-05-17 22:59:10 +0200

    Prohibits whitespace in urls.
    - Adds a whitespace check to the tumblr url validator (#79).

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.4.45")]
-[assembly: AssemblyFileVersion("1.0.4.45")]
+[assembly: AssemblyVersion("1.0.4.47")]
+[assembly: AssemblyFileVersion("1.0.4.47")]
--- src/TumblThree/TumblThree.Domain/Models/Validator.cs
+++ src/TumblThree/TumblThree.Domain/Models/Validator.cs
@@ -1,16 +1,18 @@
-﻿namespace TumblThree.Domain.Models
+﻿using System.Linq;
+
+namespace TumblThree.Domain.Models
 {
     public static class Validator
     {
         public static bool IsValidTumblrUrl(string url)
         {
-            return url != null && url.Length > 18 && url.Contains(".tumblr.com") && !url.Contains("www.tumblr.com") &&
+            return url != null && url.Length > 18 && url.Contains(".tumblr.com") && !url.Contains("www.tumblr.com") && !url.Any(char.IsWhiteSpace) &&
                    (url.StartsWith("http://", true, null) || url.StartsWith("https://", true, null));
         }
 
         public static bool IsValidTumblrLikedByUrl(string url)
         {
-            return url != null && url.Length > 31 && url.Contains("www.tumblr.com/liked/by/") &&
+            return url != null && url.Length > 31 && url.Contains("www.tumblr.com/liked/by/") && !url.Any(char.IsWhiteSpace) &&
                    (url.StartsWith("http://", true, null) || url.StartsWith("https://", true, null));
         }
     }
