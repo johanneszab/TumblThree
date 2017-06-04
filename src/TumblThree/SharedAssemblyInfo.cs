889bf50b2b67e5d5c010253fbbe8cbe9fb12208e
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-06-04 14:17:18 +0200

    Prevents scaling in fullscreen
    - Prevents scaling of the media above 100 percent in fullscreen mode.

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.4.52")]
-[assembly: AssemblyFileVersion("1.0.4.52")]
+[assembly: AssemblyVersion("1.0.4.53")]
+[assembly: AssemblyFileVersion("1.0.4.53")]
