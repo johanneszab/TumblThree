2e04f6c0312d23574686ac2d60a5c207bb11593f
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-06-22 10:38:29 +0200

    Dispose CancellationTokenSource
    - Free unmanaged resources hold by the CancellationTokenSource when the
    crawl has been stopped.
    - Fix crash if Limit Bandwidth has been set to 0 and Limit Scan
    Bandwidth is set.

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.4.59")]
-[assembly: AssemblyFileVersion("1.0.4.59")]
+[assembly: AssemblyVersion("1.0.4.60")]
+[assembly: AssemblyFileVersion("1.0.4.60")]
