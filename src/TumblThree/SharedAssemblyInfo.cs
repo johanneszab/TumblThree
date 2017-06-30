1e30efbf9293032a9960ee8c02c340399d9b3346
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-06-30 21:01:01 +0200

    Fixes dropping download speed.
    Properly handles the clean up of inaccessible files even if the queue is
    not filtered for unique input. The uncaught exception resulted in a non
    release of the semaphore reducing the effective download speed.

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.5.16")]
-[assembly: AssemblyFileVersion("1.0.5.16")]
+[assembly: AssemblyVersion("1.0.5.17")]
+[assembly: AssemblyFileVersion("1.0.5.17")]
