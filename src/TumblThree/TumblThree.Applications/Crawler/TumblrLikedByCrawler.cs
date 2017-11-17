d6e6bc2db77bc3e95bc2fdd7f112741af860fe79
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-11-17 09:22:41 +0100

    Crawler improvements
    - Improves the pagination detection in the tumblr liked/by crawler which
    results in a better download coverage.
    - Stop conditions in the tumblr liked/by and tumblr tag search crawler
    should be internationalization safe now.
    - Allows to download tumblr liked/by posts within a defined time span.
    - Improves regex in the tumblr liked/by, search and tag search crawler.

--- src/TumblThree/TumblThree.Applications/Crawler/TumblrLikedByCrawler.cs
+++ src/TumblThree/TumblThree.Applications/Crawler/TumblrLikedByCrawler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -68,7 +69,10 @@ namespace TumblThree.Applications.Crawler
                 return;
             }
 
-            foreach (int crawlerNumber in Enumerable.Range(0, shellService.Settings.ConcurrentScans))
+            long pagination = CreateStartPagination();
+
+            // TODO: find way to parallelize without losing content.
+            foreach (int crawlerNumber in Enumerable.Range(0, 1))
             {
                 await semaphoreSlim.WaitAsync();
 
@@ -76,8 +80,7 @@ namespace TumblThree.Applications.Crawler
                 {
                     try
                     {
-                        string document = await RequestDataAsync(blog.Url + "/page/" + crawlerNumber, "https://www.tumblr.com/", "https://" + blog.Name.Replace("+", "-") + ".tumblr.com");
-                        await AddUrlsToDownloadList(document, crawlerNumber);
+                        await AddUrlsToDownloadList(pagination, crawlerNumber);
                     }
                     catch
                     {
@@ -94,8 +97,21 @@ namespace TumblThree.Applications.Crawler
 
             //if (!ct.IsCancellationRequested)
             //{
-            UpdateBlogStats();
+                UpdateBlogStats();
             //}
+        }
+
+        private long CreateStartPagination()
+        {
+            long pagination = DateTimeOffset.Now.ToUnixTimeSeconds();
+            if (!string.IsNullOrEmpty(blog.DownloadTo))
+            {
+                var downloadTo = DateTime.ParseExact(blog.DownloadTo, "yyyyMMdd", CultureInfo.InvariantCulture,
+                    DateTimeStyles.None);
+                var dateTimeOffset = new DateTimeOffset(downloadTo);
+                pagination = dateTimeOffset.ToUnixTimeSeconds();
+            }
+            return pagination;
         }
 
         private async Task<bool> CheckIfLoggedIn()
@@ -104,7 +120,7 @@ namespace TumblThree.Applications.Crawler
             return !document.Contains("<div class=\"signup_view account login\"");
         }
 
-        private async Task AddUrlsToDownloadList(string document, int crawlerNumber)
+        private async Task AddUrlsToDownloadList(long pagination, int crawlerNumber)
         {
             while (true)
             {
@@ -117,35 +133,67 @@ namespace TumblThree.Applications.Crawler
                     pt.WaitWhilePausedWithResponseAsyc().Wait();
                 }
 
-                if (!string.IsNullOrWhiteSpace(blog.Tags))
-                {
-                    tags = blog.Tags.Split(',').Select(x => x.Trim()).ToList();
-                }
-
-                AddPhotoUrlToDownloadList(document);
-                AddVideoUrlToDownloadList(document);
-
-                Interlocked.Increment(ref numberOfPagesCrawled);
-                UpdateProgressQueueInformation(Resources.ProgressGetUrlShort, numberOfPagesCrawled);
-                crawlerNumber += shellService.Settings.ConcurrentScans;
-                document = await RequestDataAsync(blog.Url + "/page/" + crawlerNumber, "https://www.tumblr.com/", "https://" + blog.Name.Replace("+", "-") + ".tumblr.com");
-                if (document.Contains("<div class=\"no_posts_found\">"))
+                string document = await RequestDataAsync(blog.Url + "/page/" + crawlerNumber + "/" + pagination, "https://www.tumblr.com/", "https://" + blog.Name.Replace("+", "-") + ".tumblr.com");
+                if (document.Contains("<div class=\"no_posts_found\""))
                 {
                     return;
                 }
+
+                try
+                {
+                    AddPhotoUrlToDownloadList(document);
+                    AddVideoUrlToDownloadList(document);
+                }
+                catch (NullReferenceException)
+                {
+                }
+
+                Interlocked.Increment(ref numberOfPagesCrawled);
+                UpdateProgressQueueInformation(Resources.ProgressGetUrlShort, numberOfPagesCrawled);
+                pagination = ExtractNextPageLink(document);
+                crawlerNumber++;
+                if (!CheckIfWithinTimespan(pagination))
+                    return;
             }
+        }
+
+        private long ExtractNextPageLink(string document)
+        {
+            // Example pagination:
+            //
+            // <div id="pagination" class="pagination "><a id="previous_page_link" href="/liked/by/wallpaperfx/page/3/-1457140452" class="previous button chrome">Previous</a>
+            // <a id="next_page_link" href="/liked/by/wallpaperfx/page/5/1457139681" class="next button chrome blue">Next</a></div></div>
+            long unixTime = 0;
+            //int pageNumber = 1;
+            var pagination = "(id=\"next_page_link\" href=\"[A-Za-z0-9_/:.]+/([0-9]+)/([A-Za-z0-9]+))\"";
+            long.TryParse(Regex.Match(document, pagination).Groups[3].Value, out unixTime);
+            //int.TryParse(Regex.Match(document, pagination).Groups[2].Value, out pageNumber);
+            return unixTime;
+        }
+
+        private bool CheckIfWithinTimespan(long pagination)
+        {
+            if (!string.IsNullOrEmpty(blog.DownloadFrom))
+            {
+                var downloadFrom = DateTime.ParseExact(blog.DownloadFrom, "yyyyMMdd", CultureInfo.InvariantCulture,
+                    DateTimeStyles.None);
+                var dateTimeOffset = new DateTimeOffset(downloadFrom);
+                if (pagination < dateTimeOffset.ToUnixTimeSeconds())
+                    return false;
+            }
+            return true;
         }
 
         private void AddPhotoUrlToDownloadList(string document)
         {
             if (blog.DownloadPhoto)
             {
-                var regex = new Regex("\"(http[A-Za-z0-9_/:.]*media.tumblr.com[A-Za-z0-9_/:.]*(jpg|png|gif))\"");
+                var regex = new Regex("src=\"(http[A-Za-z0-9_/:.]*media.tumblr.com[A-Za-z0-9_/:.]*(jpg|png|gif))\"");
                 foreach (Match match in regex.Matches(document))
                 {
                     string imageUrl = match.Groups[1].Value;
                     if (imageUrl.Contains("avatar") || imageUrl.Contains("previews"))
-                        continue;
+                        continue;                    
                     if (blog.SkipGif && imageUrl.EndsWith(".gif"))
                     {
                         continue;
@@ -161,7 +209,7 @@ namespace TumblThree.Applications.Crawler
         {
             if (blog.DownloadVideo)
             {
-                var regex = new Regex("\"(http[A-Za-z0-9_/:.]*.com/video_file/[A-Za-z0-9_/:.]*)\"");
+                var regex = new Regex("src=\"(http[A-Za-z0-9_/:.]*.com/video_file/[A-Za-z0-9_/:.]*)\"");
                 foreach (Match match in regex.Matches(document))
                 {
                     string videoUrl = match.Groups[1].Value;
@@ -174,7 +222,7 @@ namespace TumblThree.Applications.Crawler
                     else if (shellService.Settings.VideoSize == 480)
                     {
                         // TODO: add valid postID
-                        AddToDownloadList(new TumblrPost(PostTypes.Video,
+                        AddToDownloadList(new TumblrPost(PostTypes.Video, 
                             "https://vt.tumblr.com/" + videoUrl.Replace("/480", "").Split('/').Last() + "_480.mp4",
                             Guid.NewGuid().ToString("N")));
                     }
